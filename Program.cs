373bcab045d3b6f2be6644a039f29f039ae085ce
shayan <shayan@UNKNOWN>
2023-05-29 12:50:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter the height: ");
+        int h=int.Parse(Console.ReadLine());
+        Console.WriteLine("enter width: ");
+        int w =int.Parse(Console.ReadLine());
+        if (h > w)
+        {
+            Console.WriteLine("this image is in portrait mode!");
+        }
+        else
+        {
+            Console.WriteLine("the image is in landscape mode!");
+        }
+    }
+}
